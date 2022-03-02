ce9a43943570e5baae724c253824875ea31c6cdf
Kyle Adams <kyleadams2600@gmail.com>
2022-03-02 14:16:01 -0500

    made second plot have units

--- main.py
+++ main.py
@@ -52,8 +52,6 @@ print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
 # Plot outputs
 plt.scatter(x_test, y_test, color="black")
 plt.plot(x_test, y_pred, color="blue", linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
+plt.title("Predicting Petal Width from Length")
 
 plt.show()
